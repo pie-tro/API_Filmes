0c917a82a71f0f1f894c94231fc56b5cfbf1073b
ClairDeLunezz <arturcruz2505@gmail.com>
2025-12-04 09:00:58 -0300

    muita coisa

--- Filmes/src/app/app.routes.ts
+++ Filmes/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { Filmes } from './filmes/filmes';
 import { Diretores } from './diretores/diretores';
+import { Home } from './home/home';
 
 export const routes: Routes = [
     {path: 'filmes', component: Filmes},
     {path: 'diretores', component: Diretores},
+    {path: '', component: Home}
 ];
